340496e4703100b8e8239fa5a14f06d8800a69ef
Micah Curtis <micahcurtis1997@gmail.com>
2022-10-14 13:17:22 -0700

    Add project files.

---
+++ EmergencyQueue_Curtis/EmergencyRoom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmergencyQueue_Curtis
+{
+    internal class EmergencyRoom
+    {
+        private PriorityQueue<string, int> _priorityQueue;
+ 
+        public EmergencyRoom()
+        {
+            _priorityQueue = new PriorityQueue<string, int>();
+
+        }
+        public void Add(string name, int priority)
+        {
+            _priorityQueue.Enqueue(name, priority);
+        }
+        public string Remove()
+        {
+            return _priorityQueue.Dequeue();
+        }
+        public List<string> List()
+        {
+            List<string> patientList = new List<string>();
+            while (_priorityQueue.Count > 0)
+            {
+                patientList.Add(_priorityQueue.Dequeue().ToString());
+            }
+            foreach (string patient in patientList)
+            {
+                _priorityQueue.Enqueue(patient, 0);
+            }
+            return patientList;
+        }        
+    }
+}
---
+++ EmergencyQueue_Curtis/Program.cs
@@ -0,0 +1,94 @@
+﻿using EmergencyQueue_Curtis;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+ConsoleKeyInfo input;
+EmergencyRoom patients = new EmergencyRoom();
+patients.Add("Mickey Mouse", 2);
+patients.Add("Fred Flintstone", 1);
+patients.Add("George Jetson", 2);
+patients.Add("Barney Rubble", 3);
+patients.Add("Johnny Quest", 1);
+
+foreach (string patient in patients.List())
+{
+    Console.WriteLine(patient.ToString());
+}
+
+Menu();
+
+do
+{
+    input = Console.ReadKey();
+    if (input.Key == ConsoleKey.D1)
+    {
+        Console.Clear();
+        string name;
+        int priority;
+        Console.Write("Enter Patient Name: ");
+        name = Console.ReadLine();
+        Console.Write("Enter Priority (1-3): ");
+        priority = int.Parse(Console.ReadLine());
+        patients.Add(name, priority);
+        Console.Clear();
+        Console.WriteLine("Patient was ADDED.");
+    }
+    else if (input.Key == ConsoleKey.D2)
+    {
+        Console.Clear();
+        patients.Remove();
+        Console.WriteLine("Top patient was REMOVED.");
+    }
+    else if(input.Key == ConsoleKey.D3)
+    {
+        Console.Clear();
+        foreach (string patient in patients.List())
+        {
+            Console.WriteLine(patient.ToString());
+        }
+    }
+    else
+    {
+        Console.Clear();
+    }
+    Menu();
+} while (input.Key != ConsoleKey.Escape);
+
+static void Menu()
+{
+    Console.WriteLine("");
+    Console.WriteLine("1. Add");
+    Console.WriteLine("2. Remove");
+    Console.WriteLine("3. List");
+    Console.WriteLine("ESC. Exit");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
